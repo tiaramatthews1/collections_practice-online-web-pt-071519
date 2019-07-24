dcd11acf55abb852e3f81371e9ef97cf6948886e
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 14:25:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,40 +1,10 @@
 def sort_array_asc(num)
-  num.sort do |a, b|
-    a <=> b
-  end
-end
-
-def sort_array_desc(num)
-
-end
-
-def sort_array_char_count(array)
- 
-end
-
-def swap_elements
   
-end
+
+
 
 def reverse_array(num)
   num.reverse
 end
 
-def kesha_maker
-  
-end
 
-def find_a(array)
-  array.find do |num|
-  num.odd?
-  end
-end
-
-
-def sum_array(num)
-  #(..).inject { |sum, n| sum + n }            
-end
-
-def add_s(array)
-  #add_s.each_with_index.collect{|array, index| }
-end
