6315650f773b61da78ac90cc5b25de99e51fd582
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 14:02:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(num)
+  num.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(num)
+
+end
+
+def sort_array_char_count(array)
+ 
+end
+
+def swap_elements
+  
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker
+  
+end
+
+def find_a(array)
+  array.find do |num|
+  num.odd?
+  end
+end
+
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
